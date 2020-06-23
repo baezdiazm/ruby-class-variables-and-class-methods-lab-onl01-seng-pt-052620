6138dce4d89088256ef24363a325f5819dd719b9
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-23 19:56:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,10 +26,27 @@ def self.genres
 end
 
 def self.genre_count
-unique = @@genres.uniq
-gencount = { unique => unique.count}
-#gencount[:genres] = unique.count
-gencount
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+    genre_count[genre] += 1
+    else
+    genre_count[genre] = 1
+    end
+  end
+  genre_count
+end
+
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+    artist_count[artist] +=1
+    else
+    artist_count[artist] = 1
+    end
+  end
+  artist_count
 end
 
 
